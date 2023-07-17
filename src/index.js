13940dd3c409fe122db5ce358342fa6fd59da600
Timothy Beler <timothybeler4@gmail.com>
2023-07-17 07:28:56 -0400

    Moved data to the utils folder. Updating the import statements next.

--- src/index.js
+++ src/index.js
@@ -4,32 +4,19 @@ import {
 	addToProjectList,
 } from "./modules/dom/domControl";
 
-import * as data from "./modules/items/data";
+import * as data from "./utils/data";
 
 // const mainPanel = document.getElementById("main-panel-wrapper");
-const allTasks = new TaskList("project-0", []);
-const project1 = new TaskList("project-1", []);
 
 // TODO loadData now returns a value instead of adding the values directly to the DOM. So the following commented out line has to be refactored into two steps.
-// window.onload = loadData("project-data");
-window.onload = function () {
-	// Check if local storage is empty
-	if (localStorage.length === 0) {
-		// Local storage is empty, handle the situation as needed
-		projectData = [allTasks, project1];
-		saveData("project-data", projectData);
-		console.log("Local storage is empty.");
-	} else {
-		// Local storage has data, you can proceed with loading and working with the data
-		data.projectData = loadData("project-data");
-		console.log("Local storage contains data.");
-	}
-};
+window.onload = data.instantiateLocalStorage();
 
 const addTaskButton = document.getElementById("add-task");
 addTaskButton.addEventListener("click", (event) => {
 	event.preventDefault();
-	data.addTask();
+	const formData = getFormData();
+	data.addTask(formData);
+	addToMainPanel();
 });
 
 const addProjectButton = document.getElementById("add-project");
@@ -50,4 +37,6 @@ dropDownProject.addEventListener("change", (event) => {
 	data.getCurrentProject();
 });
 
-// TODO change the key value to "projects" and then fill out the projects and the current project with all the tasks on load.
+function handleFormSubmit() {
+	const formData = getFormData();
+}
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,18 +1,33 @@
 import { Task } from "./modules/items/task";
-import { saveData, loadData } from "./utils/dataStorage";
-import { TaskList } from "./modules/items/taskList";
-import { getFormData } from "../dom/domControl";
+// import { saveData, loadData } from "../items";
+import { saveData, loadData } from "./dataStorage";
+import { TaskList } from "../modules/items/taskList";
+// import { getFormData } from "../dom/domControl";
+// import { getFormData } from "../modules/dom/domControl";
 
+const allTasks = new TaskList("project-0", []);
+const project1 = new TaskList("project-1", []);
 let projectData = [allTasks, project1];
 
-export function getProjectData() {
+export function getAllProjects() {
 	return projectData;
 }
 
+export function instantiateLocalStorage() {
+	// Check if local storage is empty
+	if (localStorage.length === 0) {
+		// Local storage is empty, handle the situation as needed
+		projectData = [allTasks, project1];
+		saveData("project-data", projectData);
+		console.log("Local storage is empty.");
+	} else {
+		// Local storage has data, you can proceed with loading and working with the data
+		data.projectData = loadData("project-data");
+		console.log("Local storage contains data.");
+	}
+}
 
-
-export function addTask() {
-	let formData = getFormData();
+export function addTask(formData) {
 	let newTask = new Task(formData.title, formData.date, formData.priority);
 	let currentProject = getCurrentProject();
 	// TODO change this to save the updated projectData object
