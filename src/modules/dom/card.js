b3142d85d816aab85343797875d6b8a7075a0476
Timothy Beler <timothybeler4@gmail.com>
2023-07-23 06:40:27 -0400

    Added a card object to the dom directory.

---
+++ src/modules/dom/card.js
@@ -0,0 +1,9 @@
+export function createCard(title, date, priority) {
+	const card = document.createElement("div");
+	// Create card logic here
+	return card;
+}
+
+export function styleCard() {
+	// Style card logic here
+}
